67a32411025823f469b9e05957bf4b0c3af54a75
adam.anini <adam.anini@edu.univ-eiffel.fr>
2026-04-17 18:21:16 +0200

    a

--- physique.py
+++ physique.py
@@ -1,4 +1,4 @@
-from data import Personnage, Vecteur
+from data import Personnage
 
 class Position:
     """
@@ -10,26 +10,10 @@ class Position:
         self.y = y
 
 ORIGIN = Position(0, 0)
-
-from math import sqrt
-class Vecteur:
-    """
-    (x1, y1) -> (x2, y2)
-    """
-    def __init__(self, a: Position = ORIGIN, b: Position = ORIGIN) -> None:
-        self.a = a
-        self.b = b
-        self.norme = self.calc_norme()
-
-    def calc_norme(self) -> float:
-        return sqrt(
-            (self.a.x - self.b.x) ** 2 + (self.a.y - self.b.y) ** 2
-        )
-
-
+class Vecteur: ...
 
 class MoteurPhysique:
-    def __init__(self, personnage: Personnage, gravite: Vecteur = None, vmax: Vecteur = None) -> None:
+    def __init__(self, personnage: Personnage, gravite: Vecteur | None = None, vmax: Vecteur | None = None) -> None:
         self.personnage = personnage
         self.gravite = gravite
         self.vmax = vmax
@@ -40,7 +24,4 @@ class MoteurPhysique:
 
     def onclick(self, position: tuple[int, int] = (0, 0)) -> None:
         position = self.personnage.get_vitesse()
-
         force = min(position.norme(), self.vmax)
-        
-
